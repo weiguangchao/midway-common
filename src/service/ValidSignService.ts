3522379e53a8f9c9d2fe12c653babf7262adf55f
Guangchao Wei <3409885610@qq.com>
2023-04-20 15:50:36 +0800

    support valid name

--- src/service/ValidSignService.ts
+++ src/service/ValidSignService.ts
@@ -6,12 +6,12 @@ import {
   hashPersonalMessage,
   isValidSignature,
   toBuffer,
-} from '@ethereumjs/util';
-import { ILogger, Inject, Provide, Scope, ScopeEnum } from '@midwayjs/core';
-import { toBytes } from '@noble/hashes/utils';
-import { isEthereumAddress } from '@zcloak/crypto';
-import { helpers } from '@zcloak/did';
-import { SignatureData } from '../@types/signature';
+} from "@ethereumjs/util";
+import { ILogger, Inject, Provide, Scope, ScopeEnum } from "@midwayjs/core";
+import { toBytes } from "@noble/hashes/utils";
+import { isEthereumAddress } from "@zcloak/crypto";
+import { helpers } from "@zcloak/did";
+import { SignatureData } from "../@types/signature";
 
 @Provide()
 @Scope(ScopeEnum.Singleton)
@@ -28,7 +28,7 @@ export class ValidSignService {
       const { signature, message, address: originAddress } = signatureData;
       const { r, s, v } = fromRpcSig(signature);
       this.logger.debug(
-        'from sig: %s r: %s, s: %s, v: %s',
+        "from sig: %s r: %s, s: %s, v: %s",
         signature,
         bufferToHex(r),
         bufferToHex(s),
@@ -37,7 +37,7 @@ export class ValidSignService {
 
       const msgHash = hashPersonalMessage(message);
       this.logger.debug(
-        'hash user message: %s, hash: %s',
+        "hash user message: %s, hash: %s",
         message,
         bufferToHex(msgHash)
       );
@@ -46,18 +46,18 @@ export class ValidSignService {
       const address = Address.fromPublicKey(pubkey).toString();
       const isOne = originAddress.toLowerCase() === address.toLowerCase();
       this.logger.debug(
-        'recevoer originAddress %s, address %s, equals %s',
+        "recevoer originAddress %s, address %s, equals %s",
         originAddress,
         address,
         isOne
       );
 
       const isValid = isValidSignature(v, r, s);
-      this.logger.debug('is valid sig %s', isValid);
+      this.logger.debug("is valid sig %s", isValid);
 
       return isValid && isOne;
     } catch (err) {
-      this.logger.warn('verify signature error');
+      this.logger.warn("verify signature error");
     }
     return false;
   }
@@ -65,15 +65,15 @@ export class ValidSignService {
   async parseContent(content: string): Promise<SignatureData> {
     const v1 = await this.parseContentV1(content);
     if (v1) {
-      this.logger.debug('parse v1 message ----');
+      this.logger.debug("parse v1 message ----");
       return v1;
     }
-    this.logger.debug('parse v2 message ===');
+    this.logger.debug("parse v2 message ===");
     return await this.parseContentV2(content);
   }
 
   async parseContentV1(content: string) {
-    return await this.doParseContent(content, '----');
+    return await this.doParseContent(content, "----");
   }
 
   private async doParseContent(content: string, split: string) {
@@ -84,18 +84,21 @@ export class ValidSignService {
 
       if (data.length === 2) {
         msg = undefined;
-        didSig = data[1].trim().split(',');
+        didSig = data[1].trim().split(",");
       } else {
         msg = data[0].trim();
-        didSig = data[2].trim().split(',');
+        didSig = data[2].trim().split(",");
       }
 
-      // parse address
       const didUrl = didSig[0];
+      // maybe ethereum address or valid name
       let originAddress: string = didUrl;
       if (!isEthereumAddress(didUrl)) {
-        const did = await helpers.fromDid(didUrl);
-        originAddress = did.identifier;
+        // is didUrl
+        if (didUrl.startsWith("did:zk:")) {
+          const did = await helpers.fromDid(didUrl);
+          originAddress = did.identifier;
+        }
       }
       const messageBuf = toBuffer(toBytes(msg));
 
@@ -109,15 +112,19 @@ export class ValidSignService {
         signature: sigHex,
       };
 
-      this.logger.debug('signatureData %j', signatureData);
+      this.logger.debug("signatureData %j", signatureData);
       return signatureData;
     } catch (err) {
-      this.logger.warn('parse content error');
+      this.logger.warn(
+        "parse content error, split %s, content %s",
+        split,
+        content
+      );
     }
     return null;
   }
 
   async parseContentV2(content: string) {
-    return await this.doParseContent(content, '===');
+    return await this.doParseContent(content, "===");
   }
 }
